0de9fd8fc27514911165303df32d472dc97c4e68
spicychillisus <asherben90@gmail.com>
2024-06-01 19:33:46 +0800

    studentuser.java changes

--- src/files/StudentUser.java
+++ src/files/StudentUser.java
@@ -11,7 +11,44 @@ package files;
 import javax.swing.JOptionPane;
 
 public class StudentUser {
+    
+    public static String displayMenuText() {
+        // create variables for the menu text
+        String header = "Enter your option: \n";
+        String options = "1. Display all students\n2. Search student by class\n3. Searh student by name\n4. Quit";
+        
+        String menuText = header + options;
+        String getInput = JOptionPane.showInputDialog(null, menuText); 
+        return getInput; // display
+        
+    }
+    
+    public static String checkMenuInput(int input) {
+        
+        String output = "";
+        
+        switch(input) {
+            case 1:
+                output = displayAllStudentReport();
+                break;
+            case 2:
+                output = searchStudentByClass();
+                break;
+            case 3:
+                output = searchStudentByName();
+                break;
+            case 4:
+                output = quitProgram();
+                break;
+        }
+        
+        return output;
+    }
+    
     public static void main(String[] args) {
         
+        // JOptionPane inputs always returns as string so the parseInt function is required
+        int input = Integer.parseInt(displayMenuText());
+        checkMenuInput(input);
     }
 }
